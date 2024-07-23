a100080f51987c4e6b254d7cc71b591604450120
jinance <jinance@qq.com>
2024-07-24 01:18:52 +0800

    Site updated: 2024-07-24 01:18:52

--- js/snowfall.js
+++ js/snowfall.js
@@ -1,114 +1,42 @@
-(function() {
-  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-  window.requestAnimationFrame = requestAnimationFrame;
-})();
-
-(function() {
-  var flakes = [],
-      canvas = document.createElement('canvas'),
-      ctx = canvas.getContext('2d'),
-      flakeCount = 400,
-      mX = -100,
-      mY = -100;
-
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  document.body.appendChild(canvas);
-
-  function snow() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    for (var i = 0; i < flakeCount; i++) {
-      var flake = flakes[i],
-          x = mX,
-          y = mY,
-          minDist = 150,
-          x2 = flake.x,
-          y2 = flake.y;
-
-      var dist = Math.sqrt((x - x2) * (x - x2) + (y - y2) * (y - y2)),
-          dx = x2 - x,
-          dy = y2 - y;
-
-      if (dist < minDist) {
-        var force = minDist / (dist * dist),
-            xcomp = (x - x2) / dist,
-            ycomp = (y - y2) / dist,
-            deltaV = force / 2;
-
-        flake.velX -= deltaV * xcomp;
-        flake.velY -= deltaV * ycomp;
-      } else {
-        flake.velX *= .98;
-        if (flake.velY <= flake.speed) {
-          flake.velY = flake.speed
-        }
-        flake.velX += Math.cos(flake.step += .05) * flake.stepSize;
-      }
-
-      flake.y += flake.velY;
-      flake.x += flake.velX;
-
-      if (flake.y >= canvas.height || flake.y <= 0) {
-        reset(flake);
-      }
-
-      if (flake.x >= canvas.width || flake.x <= 0) {
-        reset(flake);
-      }
-
-      ctx.fillStyle = "rgba(255,255,255," + flake.opacity + ")";
-      ctx.beginPath();
-      ctx.arc(flake.x, flake.y, flake.size, 0, Math.PI * 2);
-      ctx.fill();
-    }
-
-    requestAnimationFrame(snow);
-  };
-
-  function reset(flake) {
-    flake.x = Math.floor(Math.random() * canvas.width);
-    flake.y = 0;
-    flake.size = (Math.random() * 3) + 2;
-    flake.speed = (Math.random() * 1) + 0.5;
-    flake.velY = flake.speed;
-    flake.velX = 0;
-    flake.opacity = (Math.random() * 0.5) + 0.3;
-  }
-
-  function init() {
-    for (var i = 0; i < flakeCount; i++) {
-      var x = Math.floor(Math.random() * canvas.width),
-          y = Math.floor(Math.random() * canvas.height),
-          size = (Math.random() * 3) + 2,
-          speed = (Math.random() * 1) + 0.5,
-          opacity = (Math.random() * 0.5) + 0.3;
-
-      flakes.push({
-        speed: speed,
-        velY: speed,
-        velX: 0,
-        x: x,
-        y: y,
-        size: size,
-        stepSize: (Math.random()) / 30 * 1,
-        step: 0,
-        opacity: opacity
-      });
-    }
-
-    snow();
-  };
-
-  document.addEventListener("mousemove", function(e) {
-    mX = e.clientX,
-    mY = e.clientY
-  });
-
-  window.addEventListener("resize",function(){
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  })
-
-  init();
-})();
+/*样式一*/
+(function($){
+	$.fn.snow = function(options){
+	var $flake = $('<div id="snowbox" />').css({'position': 'absolute','z-index':'9999', 'top': '-50px'}).html('&#10052;'),
+	documentHeight 	= $(document).height(),
+	documentWidth	= $(document).width(),
+	defaults = {
+		minSize		: 10,
+		maxSize		: 20,
+		newOn		: 1000,
+		flakeColor	: "#AFDAEF" /* 此处可以定义雪花颜色，若要白色可以改为#FFFFFF */
+	},
+	options	= $.extend({}, defaults, options);
+	var interval= setInterval( function(){
+	var startPositionLeft = Math.random() * documentWidth - 100,
+	startOpacity = 0.5 + Math.random(),
+	sizeFlake = options.minSize + Math.random() * options.maxSize,
+	endPositionTop = documentHeight - 200,
+	endPositionLeft = startPositionLeft - 500 + Math.random() * 500,
+	durationFall = documentHeight * 10 + Math.random() * 5000;
+	$flake.clone().appendTo('body').css({
+		left: startPositionLeft,
+		opacity: startOpacity,
+		'font-size': sizeFlake,
+		color: options.flakeColor
+	}).animate({
+		top: endPositionTop,
+		left: endPositionLeft,
+		opacity: 0.2
+	},durationFall,'linear',function(){
+		$(this).remove()
+	});
+	}, options.newOn);
+    };
+})(jQuery);
+$(function(){
+    $.fn.snow({ 
+	    minSize: 5, /* 定义雪花最小尺寸 */
+	    maxSize: 25,/* 定义雪花最大尺寸 */
+	    newOn: 300  /* 定义密集程度，数字越小越密集 */
+    });
+});
